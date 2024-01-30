ce86c02383f8c6524d6d2148295e8440649ea379
Utsav Poudyal <cy_narrator@outlook.com>
2024-01-30 20:26:10 +0545

    Used named urlpatterns

--- projects/utsav/todo_v2/todo/urls.py
+++ projects/utsav/todo_v2/todo/urls.py
@@ -1,9 +1,9 @@
 from .views import *
 from django.urls import path
 urlpatterns = [
-    path('', home),
-    path('delete/<id>', delete),
-    path('change/<id>', change),
-    path('insert', insert),
-    path('edit/<id>', edit),
+    path('', home, name="home"),
+    path('delete/<id>', delete, name="delete"),
+    path('change/<id>', change, name="change"),
+    path('insert', insert, name="insert"),
+    path('edit/<id>', edit, name="edit"),
 ]
